ec7838985e75bf5022366f20e2e7b7d84ce50523
Milicent <cmapimha@gmail.com>
2021-10-07 01:02:14 -0400

    Initial commit

---
+++ escccar.py
@@ -0,0 +1,14 @@
+tabstring = "this is a tab\nexample\nfor\nme"
+print(tabstring)
+
+tabstringy = "1\t2\t3"
+print (tabstringy)
+
+print('I am here "no, no, \'e\'s uh.....he\'s resting".')
+
+anotherstring = """this has been
+split
+in 
+thress 
+lines"""
+print(anotherstring)
